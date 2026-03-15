2f33b49c6348712bf27247989b8c7812db56f328
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-15 18:19:27 -0300

    Debug elements erased from scripts

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "--- DEBUG ---"
-echo "Ejecutando desde: $(pwd)"
-echo "Ruta del script: $0"
-touch "dummy.txt" 
-echo "--- END DEBUG ---"
 if [[ $2 -le 0 ]] ; then
     echo "ERROR: Cantidad de clientes no permitida"
     exit 1
@@ -12,5 +7,4 @@ else
     echo "Cantidad de clientes: $2"
     python3 ./compose-file-generator/compose_file_generator.py $1 $2
 fi
-sleep 10
 exit 0
