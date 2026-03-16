cccbff11cf462e2ebbb2a12e454ed7a9189252b2
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-16 20:47:22 -0300

    Echo server finished. Waiting to test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 docker build ./echo-server-validator -t echo-server-validator:latest
 docker run --network tp0_testing_net echo-server-validator:latest
+if [[ $? -eq 0 ]] ; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
 exit 0
