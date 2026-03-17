7ccbf3f3ffe552d7757699172d912b766cdea1dc
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-16 21:03:24 -0300

    Modified conditional

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build ./echo-server-validator -t echo-server-validator:latest
 docker run --network tp0_testing_net echo-server-validator:latest
-if [[ $? -eq 0 ]] ; then
+if [ $? -eq 0 ] ; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
