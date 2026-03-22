d40e858e3db0e43b847f4f6b7c8369e3f02553af
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-21 22:59:11 -0300

    Client consults winners

--- client/common/comm_protocol.go
+++ client/common/comm_protocol.go
@@ -45,6 +45,9 @@ const END_OF_BETS_MSG_HEADER = "E"
 // Winners from server
 const WINNERS_MSG_HEADER = "W"
 
+// End of transmission
+const END_OF_BETS_HEADER = "END "
+
 // Header length in bytes
 const TOTAL_MSG_HEADER_BYTES = 2
 
