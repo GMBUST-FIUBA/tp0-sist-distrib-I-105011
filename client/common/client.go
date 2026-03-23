97e0b60b14ae4de2d59073cd0e847115e730d79b
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-22 21:47:45 -0300

    Moved logs

--- client/common/client.go
+++ client/common/client.go
@@ -162,9 +162,9 @@ func (c *Client) StartClientLoop() {
 			len(winners_list),
 		)
 	} else {
-		log.Infof("action: consulta_ganadores | result: fail")
 		fmt.Println("Error al recibir ganadores: ", err)
 		fmt.Println("Mensaje de respuesta: ", resp)
+		log.Infof("action: consulta_ganadores | result: fail")
 	}
 }
 
