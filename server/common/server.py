27b2eb4fc0f61bf52c8f563747b7e4a41e2dafef
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-23 16:36:22 -0300

    Bet manager process function implemented

--- server/common/server.py
+++ server/common/server.py
@@ -16,12 +16,121 @@ TOTAL_AGENCIES = 5
 TOTAL_THREADS_IN_POOL = 4
 
 # Bet manager process
-def bet_manager_process():
-    pass
+## Enum of commands
+class InterActorsCommand(Enum):
+    OK = 0,
+    ADD_BET = 1,
+    ADD_BATCH = 2,
+    END_TX_BETS = 3,
+    WINNERS = 4
+    ERR_NOT_ADULT = 5,
+    ERR_NOT_VALID_DOC = 6,
+    ERR_NOT_VALID_NUM = 7,
+    ERR_REPEATED_BET = 8,
+    ERR_TAKEN_BET = 9
+
+## Inter actors messages convention
+INTER_ACTOR_COMMAND_POS = 0
+INTER_ACTOR_BET_POS = 1
+INTER_ACTOR_BATCH_POS = 1
+INTER_ACTOR_END_TX_POS = 1
+INTER_ACTOR_WINNERS_POS = 1
+
+def bet_manager_process(agencies_tx_channel, agencies_rx_channel, total_agencies):
+    bets_manager = BetManager()
+    agencies_ready = set()
+
+    while True:
+        msg_from_agency = agencies_rx_channel.recv()
+        msg_type = msg_from_agency[INTER_ACTOR_COMMAND_POS]
+
+        if msg_type == InterActorsCommand.ADD_BET:
+            # Get bet
+            new_bet = msg_from_agency[INTER_ACTOR_BET_POS]
+            # Store bet
+            bets_manager.store_bet_in_database()
+            # Log result
+            logging.info(f'action: apuesta_almacenada | result: success | dni: {new_bet.document} | numero: {new_bet.number}')
+        elif msg_type == InterActorsCommand.ADD_BATCH:
+            # Get batch
+            new_bets = msg_from_agency[INTER_ACTOR_BATCH_POS]
+            # Store batch
+            bets_manager.store_bets_batch(new_bets)
+            # Log result
+            logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(new_bets)}')
+        elif msg_type == InterActorsCommand.END_TX_BETS:
+            agency = msg_from_agency[INTER_ACTOR_END_TX_POS]
+
+            # Store agency
+            agencies_ready.add(agency)
+
+            # If all agencies stopped sending bets, look for winners
+            if len(agencies_ready) == total_agencies:
+                winners_by_agency = bets_manager.load_winners(total_agencies)
+
+                # Send to all clients its winners
+                for agency, winners in winners_by_agency.items():
+                    tx_socket = agencies_tx_channel[agency]
+                    send_winners(tx_socket, winners)
 
 # Agency process
-def agency_process():
-    pass
+def agency_process(client_sock, winners_rx_channel, bets_tx_channel):
+    while True:
+        try:
+            new_message = read_message(client_sock)
+
+            # If client closes the connection
+            if new_message is None:
+                client_sock.close()
+                break
+
+            # Process bet
+            response = OK_MESSAGE
+            try:
+                command = __agency_process_message(new_message, client_sock)
+
+                if command != Command.END_TX:
+                    send_message(client_sock, response)
+
+            except WrongBatchException as e:
+                response = str(e)
+                send_message(client_sock, response)
+            except Exception as e:
+                response = str(e)
+                send_message(client_sock, response)
+                logging.error(f"action: apuesta_almacenada | result: fail | error: {e}")
+
+        except Exception:
+            client_sock.close()
+
+def __agency_process_message(self, message, client_socket):
+        command = comm_protocol.identify_command(message)
+
+        # Check type
+        if command == comm_protocol.Command.ADD_BET:
+            new_bet = create_new_bet(message)
+            self._bet_manager.store_bet_in_database(new_bet)
+            logging.info(f'action: apuesta_almacenada | result: success | dni: {new_bet.document} | numero: {new_bet.number}')
+        elif command == comm_protocol.Command.ADD_BATCH:
+            new_bets = create_new_bets_batch(message)
+            self._bet_manager.store_bets_batch(new_bets)
+            logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(new_bets)}')
+        elif command == comm_protocol.Command.END_TX:
+            agency = get_stopped_bet_sending_agency(message)
+
+            # Store agency that stopped sending data
+            self._agencies_ready.add(agency)
+            
+            # If all agencies stopped sending bets, look for winners
+            if len(self._agencies_ready) == self._total_agencies:
+                winners_by_agency = self._bet_manager.load_winners(self._total_agencies)
+
+                # Send to all clients its winners
+                for agency, winners in winners_by_agency.items():
+                    tx_socket = self._agencies_detected[agency]
+                    send_winners(tx_socket, winners)
+        
+        return command
 
 # Server class
 class Server:
