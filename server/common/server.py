8308e00893ddbbe5e82160b717360caf4e43949b
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-23 19:18:55 -0300

    Serialized socket added

--- server/common/server.py
+++ server/common/server.py
@@ -207,7 +207,7 @@ class Server:
         # Submit agency process
         self._thread_pool.apply_async(
             agency_process,
-            (c, self._bet_manager_pipe, self._bet_manager_to_worker_pipes[pipe_number][1], pipe_number)
+            (reduced_c, self._bet_manager_pipe, self._bet_manager_to_worker_pipes[pipe_number][1], pipe_number)
         )
         self._total_connected_agencies += 1
 
