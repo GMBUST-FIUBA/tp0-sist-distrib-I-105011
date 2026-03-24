1d2d0286e19bf3dd76e744bbfb54ad15abb6f079
GMBUST-FIUBA <gbustamante@fi.uba.ar>
2026-03-24 11:06:39 -0300

    Rebase errors erased

--- server/common/comm_protocol.py
+++ server/common/comm_protocol.py
@@ -42,23 +42,7 @@ command_header_to_enum = {
     ADD_BETS_BATCH_COMM_TYPE: Command.ADD_BATCH,
 }
 
-# Read command from socket
-def read_command(rx_socket):
-    message = comm_protocol.read_message(rx_socket)
-
-command_header_to_enum = {
-    ADD_BET_HEADER: Command.ADD_BET,
-    ADD_BETS_BATCH_HEADER: Command.ADD_BATCH,
-}
-
-def identify_command(message):
-    message_header = message[0:4].decode("utf-8", errors="ignore")
-
-    if message_header not in command_header_to_enum:
-        return None
-
-    parsed_message = _parse_message_single_bet(message)
-
+# Identify commands
 def identify_command(message):
     message_header = message[0]
 
